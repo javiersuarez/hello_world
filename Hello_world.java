dbdcd94070d3e291bc7330ee77fa1745865629ba
javiersuarez <javiersn2009@hotmail.com>
2015-10-07 18:01:19 +0200

    Import hello world

---
+++ Hello_world.java
@@ -0,0 +1,21 @@
+/*
+ * Hola_mundo.java
+ * 
+ * Copyright 2015 daw <daw@daw-2>
+ * 
+ */
+
+public class Hola_mundo {
+	
+	public static void main (String args[]) {
+		System.out.println ("*************************************************");
+		System.out.println ("** Hola mundo, soy el primer programa en Java **");
+		System.out.println ("*************************************************");
+		System.out.println (" |||||");
+		System.out.println ("0/ x x \\0");
+		System.out.println (" | o |");
+		System.out.println (" |\\___/|");
+		System.out.println (" |_____|");
+	}
+}
+
