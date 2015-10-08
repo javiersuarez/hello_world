8306c2924b5f64d7d390e7cb5be3e293e5ddbd85
javiersuarez <javiersn2009@hotmail.com>
2015-10-08 17:08:16 +0200

    Agregado nuevo texto como mensaje

--- Hello_world.java
+++ Hello_world.java
@@ -9,9 +9,9 @@ public class Hola_mundo {
 	
 	public static void main (String args[]) {
 		System.out.println ("*******************************************************************************************************");
-		System.out.println ("** Hello World: To forgive all is as inhuman as to forgive none (Seneca). Fortunately we have GIT **");
+		System.out.println ("** Hello World: To forgive all is as inhuman as to forgive none (Seneca). Fortunately we have GIT.   **");
 		System.out.println ("*******************************************************************************************************");
-		System.out.println (" |||||");
+		System.out.println ("Hello World: from Vilanova");
 		System.out.println ("0/ x x \\0");
 		System.out.println (" | o |");
 		System.out.println (" |\\___/|");
