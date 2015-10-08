cc95b446620708f50f4ea5c9dd33bbaf02fa4411
javiersuarez <javiersn2009@hotmail.com>
2015-10-08 17:21:47 +0200

    nuevo texto al mensaje agregado

--- Hello_world.java
+++ Hello_world.java
@@ -11,7 +11,7 @@ public class Hola_mundo {
 		System.out.println ("*************************************************");
 		System.out.println ("** Hola mundo, soy el primer programa en Java **");
 		System.out.println ("*************************************************");
-		System.out.println (" |||||");
+		System.out.println ("Hello world: from vilanova");
 		System.out.println ("0/ x x \\0");
 		System.out.println (" | o |");
 		System.out.println (" |\\___/|");
